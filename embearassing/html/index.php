399f7cb87aa0ef38fdb4a2ded51eea019a638ab3
ScubaRoomba <61845789+ScubaRoomba@users.noreply.github.com>
2020-04-13 16:44:37 -0400

    Add files via upload
    Updated embearassing to a new look.

--- embearassing/html/index.php
+++ embearassing/html/index.php
@@ -1,37 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+	define('FROM_INDEX', 1);
+	$page = empty($_GET['page'])?'intro.html':$_GET['page'];
+	function page_top($page){
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <title>Poison</title>
+	<title>Polar Bears</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.36" />
 </head>
-<body>
-<?php
-define('FROM_INDEX', 1);
-$page = empty($_GET['page'])?'home.php':$_GET['page'];
-function page_top($page){
-?>
-  <h1>Welcome to Poison!</h1>
-  <nav>
-    <ul>
-    <li><a href="?page=home.php">Home</a></li>
-    <li><a href="?page=about.php">About</a></li>
-    <li><a href="?page=contact.php">Contact Us</a></li>
-    </ul>
-  <p>Below this line is the PHP include statement</p>
-  <p>____________________________________________</p>
-<?php
-}
 
-function page_bottom(){
+<body>
+	<h1>The Polar Bear</h1>
+	<nav>
+		<ul>
+			<li><a href="?page=intro.html">Introduction</a></li>
+			<li><a href="?page=habitat.html">Habitat</a></li>
+			<li><a href="?page=diet.html">Diet and Hunting</a></li>
+		</ul>
+	</nav>
+
+<?php
+	}
+	function page_bottom(){
 ?>
-  <p>____________________________________________</p>
-  <p></p>
-  <p>Above this line is the PHP include statement</p>
-  <a href="?page=reset.php">Reset Apache Logs</a>
+<br><p>If you find any problems or misinformation on our web page, please contact us at steve@larrynet.app!</p><br>
 </body>
 </html>
-
 <?php
 }
 register_shutdown_function('page_bottom');
